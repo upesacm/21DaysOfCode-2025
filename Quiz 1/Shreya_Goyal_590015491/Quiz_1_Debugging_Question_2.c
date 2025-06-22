84b9d3de47a90b1c6c7f8dd6fc7db8f6a69fc8bc
Shreya Goyal <shreyagoyal2128@gmail.com>
2025-06-22 23:18:49 +0530

    Added Quiz 1 Question - Shreya Goyal 590015491

---
+++ Quiz 1/Shreya_Goyal_590015491/Quiz_1_Debugging_Question_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int findMax(int arr[], int n) 
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++) 
+    {
+        if (arr[i] > max) 
+       {
+            max = arr[i];
+        }
+    }
+    return max;
+}
