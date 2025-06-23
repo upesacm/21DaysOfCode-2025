56e47c43684ce29f2ac5c116a331764251d1c443
Tushar Mishra <tusharmishra587@gmail.com>
2025-06-23 23:17:10 +0530

    Create Day_7_question_2.py

---
+++ DSA/Tushar_590016817/Day_7_question_2.py
@@ -0,0 +1,8 @@
+def count_character_occurrences(s, ch):
+    count = s.count(ch)
+    print(count)
+
+# Test examples
+count_character_occurrences("programming", 'r')
+count_character_occurrences("Hello World", 'l')
+count_character_occurrences("JavaScript", 'a')
