bf2da02780fb907eafa8ccff2e0e23228db162b0
Anshul Jain <anshuljn17@gmail.com>
2025-06-29 20:24:41 +0530

    Synced with upstream

--- Quiz_2/gehna_khanna_590013140/Quiz_2_Debugging_Question_1.py
+++
@@ -1,8 +0,0 @@
-#) Empty string should return False since it contains no digits
-def is_digits_only(s):
-    if len(s) == 0:
-        return False 
-    for char in s:
-        if char < '0' or char > '9':
-            return False
-    return True
